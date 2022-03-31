5e8a39cc83a5242d5101db6ed838be32526b316a
jaisonds <jaisondsouzasuhas@gmail.com>
2022-03-31 10:16:20 +0530

    mytest

---
+++ t1.sh
@@ -0,0 +1,8 @@
+echo "enter ur age"
+read age
+if [ $age -ge 60 ]
+then
+echo "adult"
+else echo "teen"
+fi
+
---
+++ test.sh
@@ -0,0 +1,43 @@
+echo "enter the age "
+read age
+if
+ ["$age" -ge 21 ]; 
+then
+ echo "senior"
+else
+echo "sorry"
+fi 
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+A
+
